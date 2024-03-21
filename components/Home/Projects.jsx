6f745dadecd82a9705050daa4e65a25339c9f933
steve-chude <125799928+steve-chude@users.noreply.github.com>
2024-03-21 14:01:58 +0100

    updated projects section

--- components/Home/Projects.jsx
+++ components/Home/Projects.jsx
@@ -6,45 +6,52 @@ import "aos/dist/aos.css";
 
 const listProjects = [
   {
-    image: "/projects/blog.png",
-    title: "Blog Design",
+    image: "/projects/portfolio.png",
+    title: "Steve's Portfolio",
     description:
       "This is a Blog website built with next.js and styled with tailwind css. this was a personal project i built off hand without any pre-made designs. deployed on vercel",
-    link: "https://blog-by-steve.vercel.app/",
+    link: "https://steve-chude.vercel.app/",
+  },
+  {
+    image: "/projects/bottleking.png",
+    title: "BottleKing Website",
+    description:
+      "I worked on the frontend of this website for Bottleking. It's an e-commerce web app for their products and services, where users can sign up for an account, place orders, etc.",
+    link: "https://bottleking.ng/",
   },
   {
     image: "/projects/cryptobase.png",
     title: "A Crypto Monitoring App",
     description:
-      "This is a Blog website built with next.js and styled with tailwind css. this was a personal project i built off hand without any pre-made designs. deployed on vercel",
+      "A responsive crypto website fetched from coingecko’s api as third-party, built with react js and styled with tailwind css, with an addition of a firebase backend with full user authentications and route protections.",
     link: "https://cryptobase-website.netlify.app/",
   },
   {
     image: "/projects/foodmine.png",
-    title: "Food Ordering App",
+    title: "Food Ordering App (In Progress)",
     description:
-      "This is a Blog website built with next.js and styled with tailwind css. this was a personal project i built off hand without any pre-made designs. deployed on vercel",
+      "A foodmine website built with angular, it represents a food commerce app with the following features (view all available foods, view foods by categories, click on a particular food, add food to cart, view cart page with all foods added to cart). It is still missing an API server for user authentication (sign up and login, payment and checkout page) though, but it is an awesome representation of a food-commerce web application.",
     link: "https://foodmine-app.vercel.app/",
   },
   {
     image: "/projects/gift-card.png",
     title: "Gift Cards App",
     description:
-      "This is a Blog website built with next.js and styled with tailwind css. this was a personal project i built off hand without any pre-made designs. deployed on vercel",
+      "A fully responsive landing page of a GiftCard website from Figma Design, built and styled in 6 days with Next Js and TailwindCSS. Used GSAP library for animations.",
     link: "https://gift-card-phi.vercel.app/",
   },
   {
     image: "/projects/payslate.png",
     title: "Payslate Organizations App",
     description:
-      "This is a Blog website built with next.js and styled with tailwind css. this was a personal project i built off hand without any pre-made designs. deployed on vercel",
+      "A modern payment solution app for Organizations. A company can open a payslate account to easily receive payments from their customers, easily make withdrawals and manage their business accounts. Sub accounts can be created as well for split payments. I advice yout to sign up and check it out.",
     link: "https://organization.payslate.com.ng/",
   },
   {
     image: "/projects/payslate2.png",
     title: "Payslate",
     description:
-      "This is a Blog website built with next.js and styled with tailwind css. this was a personal project i built off hand without any pre-made designs. deployed on vercel",
+      "A modern payment solution app that has 2 user journeys (Organization & Users). A company can open a payslate account to easily receive payments from their customers, while an individual can open a payslate account to easily connect with multiple organizations registered on the platform and make payments to them.",
     link: "https://payslate.com.ng/",
   },
 ];
